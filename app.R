7199f1b7be810f355f13b0f1d3e6b29cead653a6
alejanet <alexandrajanr@gmail.com>
2024-02-17 02:49:36 -0500

    without token

--- app.R
+++ app.R
@@ -11,15 +11,15 @@ library(tidyverse)
 
 #### get working directory
 getwd()
-setwd("/Users/Usuario/OneDrive - Grupo de Análisis para el Desarrollo/Documentos/Proyecto Creer/Fase 2/CREER_monitoreo/0_git/creer-db1")
+setwd("/Users/Usuario/")
 
 #### Google credentials
 gs4_deauth()
-googlesheets4::gs4_auth(path = './token/turing-lane-413803-27f19c8788ec.json')
+googlesheets4::gs4_auth(path = './YYYYYYYYYYYYYY.json')
 
 #### Read google sheets data into R
-#(1) share with spreadsheet-user-creer-1@turing-lane-413803.iam.gserviceaccount.com
-df <- read_sheet('https://docs.google.com/spreadsheets/d/17ViTAMf0aJnfDL271QkO6wVeH5htwKirz7Ek9h0id8g/edit#gid=0', sheet = "Reporte")
+#(1) share with XXXXXXXXXXXXXX@WWWWWWWWWWWWWWWWW.iam.gserviceaccount.com
+df <- read_sheet('URL', sheet = "Reporte")
 
 #### Cleaning data
 head(df)
@@ -34,22 +34,14 @@ new_df <- df %>%
          ndocentes="Indique el número de docentes que permanecen en la sesión", cumple="¿En qué medida se cumplió con las actividades planificadas?"
          )
 
-## Drop NA observations(missing)
-#new_df <- new_df[!is.na(new_df$prov),]
-#new_df <- new_df[!is.na(new_df$ap),]
-
 ## Keep a group of variables
 new_df <- select(new_df, marca, prov, ap, ie, nomdocente, implementa, fecha_visita, nsesion, grados_atendidos, area, lidera,
              ndocentes, cumple)
-## Me quedo con obs nuevas
-#new_df <- subset(new_df, !grepl("Opción", ap))
-
-# Clear a data frame from memory
-#rm(new_df)
 
 #turn into a factor variable
 v_ie <- unlist(new_df$ie)
 new_df$v_ie <- v_ie
+
 new_df <- new_df[order(new_df$v_ie), ]
 f_ie <- as.factor(new_df$v_ie)
 new_df$f_ie <- f_ie
@@ -144,8 +136,6 @@ server <- function(input, output, session) {
   proxy <- dataTableProxy("table")
   
   #when data changes, replace data using server-side proxy
-  #set resetPaging/clearSelection to FALSE to avoid loosing interactivity mechanics
-  #https://rdrr.io/cran/DT/man/replaceData.html
   observe({
     replaceData(
       proxy,
